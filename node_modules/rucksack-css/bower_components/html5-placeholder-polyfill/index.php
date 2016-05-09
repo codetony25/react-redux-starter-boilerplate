52ab1ad13b80a8f27e1762ced90c4535bc4c1da7
Tony Nguyen <tony.nguyen@doclermedia.com>
2016-05-09 11:53:24 -0700

    removed node_modules

--- node_modules/rucksack-css/bower_components/html5-placeholder-polyfill/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-// just needed for the demo web
-echo file_get_contents('./web/readme.html');
-?>
--- node_modules/stylelint-webpack-plugin/test/testfiles/test1/index.js
+++
@@ -1,3 +0,0 @@
-require(path.join(__dirname, './../../../node_modules/file-loader/index') + '!./test.scss');
-
-console.log('test1');
